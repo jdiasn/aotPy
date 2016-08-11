1b7363dec7e8dd7af124fb5dd3674eb5ab53a7c5
jdiasn <jdiasn@gmail.com>
2016-08-11 16:09:42 -0300

    Two new functions, first to get first and last julian day for each month and second to get file list between two julian days.

--- extractLib.py
+++ extractLib.py
@@ -1,4 +1,8 @@
 import numpy as np
+import pandas as pd
+
+import calendar
+import datetime
 
 def DoDegResolutionX(deltaX):
 
@@ -14,5 +18,26 @@ def DoDegResolutionY(deltaY):
     latRes = (deltaY * 360)/ (2 * np.pi * earthRay)
     
     return latRes
+
+
+def getMonthFirstAndLastDay(year,month):
+
+    weekDayBegin, lenMonth = calendar.monthrange(int(year),int(month))
+    beginDay = datetime.datetime.strptime(year+month+'01','%Y%m%d').strftime('%Y%j')
+    endDay = datetime.datetime.strptime(year+month+str(lenMonth),'%Y%m%d').strftime('%Y%j')
+
+    return beginDay, endDay
+
+
+def getFileBetween2Days(beginDay,endDay,fileList):
+
+    dataFrame = pd.DataFrame(fileList)
+    dataFrame = dataFrame[(dataFrame[0].str.split('/').str[-1].str.split('.').str[1].str[1:] >= beginDay )]
+    dataFrame = dataFrame[(dataFrame[0].str.split('/').str[-1].str.split('.').str[1].str[1:] <= endDay)]
+
+    fileList = list(dataFrame[0])
+
+    return fileList
+
 
 
